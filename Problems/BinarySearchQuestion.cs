5b7af4ba8ae1e08ccb33bc9c842b4bfc088949b6
bicyclemark <mark.d.wardell@gmail.com>
2020-10-17 19:03:32 -0500

    all in

---
+++ Problems/BinarySearchQuestion.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework.Constraints;
+using System;
+using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
+using System.Text;
+
+namespace Problems
+{
+    public class BinarySearchQuestion
+    {
+        public static int BinarySearch(int[] array, int target)
+        {
+            return BinarySearch(array, target, 0, array.Length - 1);
+        }
+        public static int  BinarySearch(int []array,int target, int left, int right)
+        {
+            while (left <= right)
+            {
+                int middle = (left + right) / 2;
+                int match = array[middle];
+                if (match == target)
+                {
+                    return middle;
+                }
+                else if (target < match)
+                {
+                    right = middle - 1;
+                }
+                else
+                    left = middle + 1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ TestAlgoExpert/TestBinarySearch.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Problems;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestAlgoExpert
+{
+
+    [TestFixture]
+    public class TestBinarySearch
+    {
+        [TestCase(3, 33,  0, 1, 21, 33, 45, 45, 61, 71, 72, 73)]
+        [Test]
+        public void BinarySearch(int expected, int target,  params int[] array)
+        {
+            int nPass = BinarySearchQuestion.BinarySearch(array, target);
+            Assert.AreEqual(expected, nPass);
+        }
+/*
+        [TestCase(3, 33, 0, 1, 21, 33, 45, 45, 61, 71, 72, 73)]
+        [Test]
+        public void BinarySearch(int expected, int target, params int[] array)
+        {
+            int nPass = BinarySearchQuestion.BinarySearch(array, target);
+            Assert.AreEqual(expected, nPass);
+        }
+*/
+    }
+}
